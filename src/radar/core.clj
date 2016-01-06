606a1cab89bd939b8f473165804e750bbc3d1c1b
Greg Allen <GregA100K@gmail.com>
2016-01-05 23:15:31 -0600

    fix the core call to draw the radar to take into account if the level names a included in the json file

--- src/radar/core.clj
+++ src/radar/core.clj
@@ -8,4 +8,7 @@
   (let [config-file (first args)
         output-file (str (first (str/split config-file #"\.")) ".png")
         configuration (read-configuration config-file)]
-    (draw-radar (categories configuration) (targets configuration) (levels configuration) output-file)))
+    (if (levels configuration)
+    (draw-radar (categories configuration) (targets configuration) (levels configuration) output-file)
+    (draw-radar (categories configuration) (targets configuration) output-file)
+)))
